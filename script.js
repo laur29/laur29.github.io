8bcfabd1241c7dfb41e027fa4a1f3248da9e444a
lauren292023F <lauren29@vt.edu>
2023-12-08 16:15:01 -0500

    iofjfj

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function openContact() {
-  document.getElementById("contactMe").style.display = "block";
-}
-function closeContact() {
-    document.getElementById("contactMe").style.display = "none";
-  }
